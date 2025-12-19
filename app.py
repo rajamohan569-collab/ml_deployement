16c664fce1127e1773967985e0947bcb24bd0216
rajamohan569-collab <rajamohan569@gmail.com>
2025-12-19 13:27:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ st.title('Affected Population in Disasters')
 latitude = int(st.number_input('latitude',value=00.00))
 longitude = int(st.number_input('longitude',value=00.00))
 severity_level = int(st.number_input('severity_level',value=00.00))
-affected_population = st.number_input("Affected Population", min_value=00.00)
 estimated_economic_loss_usd   = int(st.number_input('estimated_economic_loss_usd',value=00.00))
 response_time_hours = int(st.number_input('response_time_hours',value=00.00))
 infrastructure_damage_index = int(st.number_input('infrastructure_damage_index',value=00.00))
@@ -29,7 +28,6 @@ if st.button('affected_population % '):
         'latitude':latitude,
         'longitude':longitude,
         'severity_level':severity_level,
-        'affected_population':affected_population,
         'estimated_economic_loss_usd':estimated_economic_loss_usd,
         'response_time_hours':response_time_hours,
         'infrastructure_damage_index':infrastructure_damage_index,
@@ -40,3 +38,4 @@ if st.button('affected_population % '):
     result = model.predict(input_df)[0]
 
     st.success(f'affected_population % :{result:.2f}')
+
